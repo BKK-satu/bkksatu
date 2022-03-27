327e94f48c806b602a3f9ed6c62a5ea72baecf8e
akwancakra <akwancakra20@gmail.com>
2022-03-27 12:32:28 +0700

    Membuat sidebar responsive dengan php

--- resources/views/layouts/sidebar-admin.blade.php
+++ resources/views/layouts/sidebar-admin.blade.php
@@ -6,32 +6,37 @@
         </a>
         <a class="{{ $title == 'dashboard' ? 'nav-active ' : '' }}text-decoration-none text-black navbar-item text-center d-block py-2 my-2"
             href="/ad/dashboard">
-            <i class='bx bx-grid-alt align-middle text-center d-block'></i>
+            <i
+                class='bx {{ $title == 'dashboard' ? 'bxs-grid-alt ' : 'bx-grid-alt ' }}align-middle text-center d-block'></i>
             <span>Dashboard</span>
         </a>
         <a class="{{ $title == 'alumni' ? 'nav-active ' : '' }}text-decoration-none text-black navbar-item text-center d-block py-2 my-2"
             href="/ad/al/main">
-            <i class='bx bx-book-reader align-middle text-center d-block'></i>
+            <i
+                class='bx {{ $title == 'alumni' ? 'bxs-book-reader ' : 'bx-book-reader ' }}align-middle text-center d-block'></i>
             <span>Alumni</span>
         </a>
         <a class="{{ $title == 'akun' ? 'nav-active ' : '' }}text-decoration-none text-black navbar-item text-center d-block py-2 my-2"
             href="/ad/ak/main">
-            <i class='bx bx-user-circle align-middle text-center d-block'></i>
+            <i
+                class='bx {{ $title == 'akun' ? 'bxs-user-circle ' : 'bx-user-circle ' }}align-middle text-center d-block'></i>
             <span>Akun</span>
         </a>
         <a class="{{ $title == 'informasi' ? 'nav-active ' : '' }}text-decoration-none text-black navbar-item text-center d-block py-2 my-2"
             href="/ad/in/main">
-            <i class='bx bx-news align-middle text-center d-block'></i>
+            <i class='bx {{ $title == 'informasi' ? 'bxs-news ' : 'bx-news ' }}align-middle text-center d-block'></i>
             <span>Informasi</span>
         </a>
         <a class="{{ $title == 'loker' ? 'nav-active ' : '' }}text-decoration-none text-black navbar-item text-center d-block py-2 my-2"
             href="/ad/lk/main">
-            <i class='bx bx-briefcase-alt-2 align-middle text-center d-block'></i>
+            <i
+                class='bx {{ $title == 'loker' ? 'bxs-briefcase-alt-2 ' : 'bx-briefcase-alt-2 ' }}align-middle text-center d-block'></i>
             <span>Loker</span>
         </a>
         <a class="{{ $title == 'mitra' ? 'nav-active ' : '' }}text-decoration-none text-black navbar-item text-center d-block py-2 my-2"
             href="/ad/mt/main">
-            <i class='bx bx-building-house align-middle text-center d-block'></i>
+            <i
+                class='bx {{ $title == 'mitra' ? 'bxs-building-house ' : 'bx-building-house ' }}align-middle text-center d-block'></i>
             <span>Mitra</span>
         </a>
         <div class="navbar-logout text-center d-block py-3">
